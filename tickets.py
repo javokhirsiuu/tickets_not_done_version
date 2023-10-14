300a6aea8d16045e5026a1887bac844bf2736f18
javohir makhmudjanov <137611918+javokhirsiuu@users.noreply.github.com>
2023-10-14 23:24:02 +0500

    Update tickets.py

--- tickets.py
+++ tickets.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter,Depends
+from fastapi import APIRouter,Depends,H
 import random
 from sqlalchemy.orm import Session
 from ticket_modul import Tickets,Questions
@@ -10,9 +10,12 @@ router = APIRouter()
 num_questions_per_ticket = 20
 num_tickets = 50
 @router.get("/get_ticket/{ticket_number}")
-async def get_ticket(ticket_number: int):
-    if ticket_number < 1 or ticket_number > num_tickets:
+async def get_ticket(ticket_number: int,db:Session=Depends(get_db)):
+    db = db.query(Tickets).filter(Tickets.id ==ticket_number)
+    if not db:
         return {"error": "Билет с таким номером не существует"}
+    else:
+        return { ticket_number: Questions}
 
 
 @router.post("add/ticket/")
@@ -33,4 +36,63 @@ async def add(question:question,db:Depends(get_db)):
     db.add(db_modul)
     db.commit()
     return db_modul
+
+@router.put("change/ticket")
+async def update_ticket(updated_name:Updated_ticket,db:Session = Depends(get_db)):
+    db = db.query(Tickets).filter(Tickets.name==updated_name).first()
+    if not db:
+        raise HTTPException(status_code = 404,detail ="not exists")
+    else:
+        db_modul = Ticketss
+        db_modul.name = updated_name.updated_name
+        db.add(db_modul)
+        db.refresh()
+        db.commit()
+        return db_modul
+@router.put("change/ticket")
+async def update_questio(updated_name:Updated_ticket,db:Session = Depends(get_db)):
+    db = db.query(Tickets).filter(Tickets.name==updated_name).first()
+    if not db:
+        raise HTTPException(status_code = 404,detail ="not exists")
+    else:
+        db_modul = Ticketss
+        db_modul.name = updated_name.updated_name
+        db.add(db_modul)
+        db.refresh()
+        db.commit()
+        return db_modul
+
+@router.put("change/ticket")
+async def update_ticket(updated_data:updated_question,db:Session = Depends(get_db)):
+    db = db.query(Tickets).filter(Tickets.name==updated_data).first()
+    if not db:
+        raise HTTPException(status_code = 404,detail ="not exists")
+    else:
+        db_modul = Questions
+        db_modul.name = updated_data.updated_name
+        db_modul.question = updated_data.question
+        db.add(db_modul)
+        db.refresh()
+        db.commit()
+        return db_modul
+
+
+@router.delete("api/delete")
+async def delete_(id:int,question:question,ticket:Ticketss,db:Session=Depends(get_db)):
+    db = db.query(Tickets).filter(Tickets.id ==id).first()
+    if not db:
+        raise HTTPException(status_code =404,detail="not found")
+    else:
+        db_modul = Tickets
+        db_modul1 = Questions
+        db_modul.id = id
+        db_modul.name = ticket.name
+        db_modul1.question = question.question
+        db.delete(db_modul)
+        db.delete(db_modul1)
+        return "deleted"
+
+
+
+        
 
