39abe40158f32eb67881871ad2ff5d05c2700e3d
javohir makhmudjanov <137611918+javokhirsiuu@users.noreply.github.com>
2023-10-14 23:26:27 +0500

    Update ticket_modul.py

--- ticket_modul.py
+++ ticket_modul.py
@@ -3,12 +3,11 @@ from sqlalchemy import Column,Integer,String
 
 class Tickets(BaseModel):
     __tablename__ = "tickets"
-    class Config:
-        ignored_types = type[int]
-    id = Column(Integer,primary_key=True,nullable=False)
+    id = Column(Integer,primary_key=True,index=True)
     name = Column(String,nullable=False)
 
 class Questions(BaseModel):
-    id = Column(Integer,primary_key=True,nullable=False)
+    __tablename__ = "questions"
+    id = Column(Integer,primary_key=True,index=True)
     question = Column(String,nullable=False)
     name = Column(String,nullable=False)
