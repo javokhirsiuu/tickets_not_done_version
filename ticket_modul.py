e8b5a71a8eeee0cf45c1031d75b191ff7f2c5d5c
javohir makhmudjanov <137611918+javokhirsiuu@users.noreply.github.com>
2023-11-05 21:15:47 +0500

    Update ticket_modul.py

--- ticket_modul.py
+++ ticket_modul.py
@@ -1,15 +1,1 @@
-from pydantic import BaseModel
-from sqlalchemy import Column,Integer,String
-from typing import ClassVar
 
-class Tickets(BaseModel):
-    __tablename__ = "tickets"
-    id:ClassVar[int] = Column(Integer, primary_key=True)
-    name:ClassVar[str] = Column(String)
-
-
-class Questions(BaseModel):
-    __tablename__ = "questions"
-    id: ClassVar[int] = Column(Integer, primary_key=True)
-    name: ClassVar[str] = Column(String)
-    question:ClassVar[str] = Column(String)
