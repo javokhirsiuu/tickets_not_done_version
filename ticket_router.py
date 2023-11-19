7816d3e15e9caed48524daeb3ebca392809ab110
javohir makhmudjanov <137611918+javokhirsiuu@users.noreply.github.com>
2023-11-19 18:12:03 +0500

    Update ticket_router.py

--- ticket_router.py
+++ ticket_router.py
@@ -2,59 +2,47 @@ from fastapi import Depends, HTTPException, APIRouter
 from sqlalchemy.orm import Session
 from db import get_db
 from models import Ticket
-from schemas import Ticket_create
+from schemas import Ticket_create,ticket_update
 import random
-from config import AUTH_USE_REQUEST_HEADERS
+
 router = APIRouter()
 
-AUTH_USE_REQUEST_HEADERS
 @router.get("/tickets/")
 async def get_tickets(db=Depends(get_db)):
     users = db.query(Ticket).all()
     return users
 @router.post("/tickets/")
-async def create_ticket(ticket: Ticket_create,db: Session = Depends(get_db)):
+async def create_ticket(ticket: Ticket_create,db = Depends(get_db)):
     check = db.query(Ticket).filter(Ticket.tickets_name == ticket.name).first()
     if check is not None:
-        raise HTTPException(status_code=404,
-                            detail='already exists')
-
+        raise HTTPException(status_code=404,detail='already exists')
 
     db_modul = Ticket()
     db_modul.tickets_name = ticket.name
+    # db.refresh(db_modul)
     db.add(db_modul)
     db.commit()
-    db.refresh(db_modul )
     return db
 
-@router.get("/question")
-async def get_question(id:int,db = Depends(get_db)):
-    check = db.query(Ticket).filter(Ticket.tickets_id==id).first()
-    if not check:
-        raise HTTPException(status_code=404,detail="does not exist")
-    else:
 
-        return {random.choice(check)}
 
 @router.get("/{ticket_id}")
 async def get_ticket(id: int, db=Depends(get_db)):
     user = db.query(Ticket).filter(Ticket.tickets_id == id).first()
     if not user:
-        raise HTTPException(status_code=404, detail="User not found")
+        raise HTTPException(status_code=404, detail=" not found")
     return user
-@router.put("/edit-ticket")
-async def ticket_edit(id,schema:str ,db = Depends(get_db)):
 
-    model = db.query(Ticket).filter(Ticket.tickets_id == id).first()
-    if model is None:
-        raise HTTPException(status_code=404, detail="not found")
+@router.put("/edit/ticket")
+async def edit(ticket_id:int,ticket_new:str,db=Depends(get_db)):
+    db_ticket = db.query(Ticket).filter(Ticket.tickets_id == ticket_id).first()
+    if not db_ticket:
+        raise HTTPException(status_code=404, detail="Ticket not found")
     else:
-        model.name = schema
-        db.add(model)
+        db_ticket.tickets_name=ticket_new
         db.commit()
-        db.refresh(model)
-        return model
-
+        db.refresh(db_ticket)
+        return db_ticket
 
 
 @router.delete("/tickets/{ticket_id}/")
