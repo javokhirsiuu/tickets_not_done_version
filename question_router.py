7645c03fc2e8751772810e88c9986d5e25fb61f8
javohir makhmudjanov <137611918+javokhirsiuu@users.noreply.github.com>
2023-11-19 18:10:47 +0500

    Update question_router.py

--- question_router.py
+++ question_router.py
@@ -1,50 +1,65 @@
-from fastapi import APIRouter,Depends,HTTPException
-from db import get_db,engine,Base,create_engine
-from models import Question,Ticket
-from sqlalchemy.orm import Session
-from schemas import Question_create
+from fastapi import APIRouter, Depends, HTTPException
+from db import get_db, engine, Base
+from models import Question
 import random
 Base.metadata.create_all(engine)
 
 router = APIRouter()
 
-@router.get("/questions/")
-async def get_questions(db=Depends(get_db)):
-    chec = db.query(Question).all
-    return chec
+
+@router.get("/all/question")
+async def all_question(db=Depends(get_db)):
+    query1 = db.query(Question).all()
+    return query1
 
 
-@router.get("/questions/random/{ticket_id}/")
-async def get_random_question(ticket_id: int, db = Depends(get_db)):
-    check = db.query(Question).filter(Question.ticket_id == ticket_id).all()
-    if not check:
-        raise HTTPException(status_code=404, detail="No questions found for this ticket.")
+@router.get("/question/{id}")
+async def get_question(id: int, db=Depends(get_db)):
+    query = db.query(Question).filter(Question.questions_id == id).first()
+
+    if not query:
+        raise HTTPException(status_code=404, detail="tupoymisan")
+
+    return query
+
+
+@router.post("/add")
+async def create(question: str, db=Depends(get_db)):
+    model = Question()
+    model.questions_name = question
+    db.add(model)
+    db.commit()
+    return model
+@router.get("/random/question")
+async def get_random(db=Depends(get_db)):
+    ticket = db.query(Question).all()
+    if not ticket:
+        raise HTTPException(status_code=404, detail="No")
+
+    random_ticket = random.choice(ticket)
+
+    return random_ticket
+
+
+@router.put("/edit/question/")
+async def edit(id: int, update: str, db=Depends(get_db)):
+    updater = db.query(Question).filter(Question.questions_id == id).first()
+    if not updater:
+        raise HTTPException(status_code=404, detail="try again")
+
     else:
-        random_question = random.choice(check)
-        return {"question_text": random_question.questions}
+        updater.questions_name = update
+        db.add(updater)
+        db.commit()
+        db.refresh(updater)
+        return updater
 
 
-@router.post("/add/questions/to/ticket")
-def create_question(question:Question_create, db = Depends(get_db)):
-    chec = db.query(Ticket).filter(Ticket.tickets_id == question.ticket_id).first()
-
-    if not chec:
-        raise HTTPException(status_code=404, detail=" not found")
-
-    db_comment = Question()
-    db_comment.ticket_id= question.ticket_id
-    db_comment.questions = question.question
-    db.add(db_comment)
-    db.commit()
-    db.refresh(db_comment)
-
-    return db_comment
-
-@router.delete("/tickets/{ticket_id}/questions/")
-def delete_questions(ticket_id: int, db: Session = Depends(get_db)):
-    deleted_count = db.query(Question).filter(Question.ticket_id == ticket_id).first()
-    if not deleted_count:
-        raise HTTPException(status_code=404, detail="No questions found for this ticket")
-    db.delete(deleted_count)
-    db.commit()
-    return {"message": f"Deleted {deleted_count} questions"}
+@router.delete("/delete")
+async def delete_com(ID: int, db=Depends(get_db)):
+    comment = db.query(Question).filter(Question.questions_id == ID).first()
+    if not comment:
+        raise HTTPException(status_code=404, detail="comment not found")
+    db.delete(comment)
+    db.refresh(comment)
+    return "comment deleted successfully"
